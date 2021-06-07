7e6c8d7893b805d774b1e7c6caa7b68c0fdd0423
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-06-07 09:17:22 +0200

    Add files via upload

---
+++ Ponavljanje1/Ponavljanje1/KlasaG.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje1
+{
+    class KlasaG
+    {
+        public int Plus10(int Brojevi)
+        {
+            int Plus10 =Brojevi+10 ;
+            return Plus10;
+        }
+        public int f50(int Zbroj)
+        {
+            int f50 = 50;
+            return f50;
+        }
+    }
+}
---
+++ Ponavljanje1/Ponavljanje1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  Brojevi, Zbroj;
+            int[] Broj = new int[1];
+
+
+            Console.Write("Upisite broj : ");
+            Brojevi = Convert.ToInt32(Console.ReadLine());
+
+            KlasaG Objekt = new KlasaG();
+            Brojevi = Objekt.Plus10(Brojevi) ;
+            Zbroj = Objekt.f50(Brojevi);
+            Console.WriteLine("Plus10 : " + Brojevi + " f50 : " + Zbroj);
+
+            Console.ReadKey();
+        }
+    }
+}
